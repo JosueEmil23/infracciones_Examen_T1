749c4d53d2c0b1c06d7137f68bf9b703df5eddbe
Josue Emilio <josueemil09@gmail.com>
2026-03-31 02:20:50 -0500

    pregunta 04: Bloquear Pendiente

--- src/test/java/edu/pe/cibertec/infracciones/MultaServiceTest.java
+++ src/test/java/edu/pe/cibertec/infracciones/MultaServiceTest.java
@@ -1,5 +1,6 @@
 package edu.pe.cibertec.infracciones;
 
+import edu.pe.cibertec.infracciones.exception.InfractorBloqueadoException;
 import edu.pe.cibertec.infracciones.model.EstadoMulta;
 import edu.pe.cibertec.infracciones.model.Infractor;
 import edu.pe.cibertec.infracciones.model.Multa;
@@ -11,6 +12,7 @@ import edu.pe.cibertec.infracciones.service.impl.MultaServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -20,6 +22,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -67,5 +70,36 @@ public class MultaServiceTest {
 
         assertEquals(infractorB, resultado.getInfractor());
         verify(multaRepository, times(1)).save(multa);
+    }
+
+
+    @Test
+    @DisplayName("Arrojar Excepcion cuando el infractor esta bloqueado")
+    void givenInfractorBloqueado_whenTransferirMulta_thenThrowsExceptionAndSaveNotCalled(){
+        ArgumentCaptor <Multa> multaCaptor = ArgumentCaptor.forClass(Multa.class);
+
+        Vehiculo vehiculo = new Vehiculo();
+        vehiculo.setId(1L);
+
+        Infractor infractorA = new Infractor();
+        infractorA.setId(1L);
+
+        Infractor infractorB = new Infractor();
+        infractorB.setId(2L);
+        infractorB.setBloqueado(true);
+
+        Multa multa = new Multa();
+        multa.setId(1L);
+        multa.setEstado(EstadoMulta.PENDIENTE);
+        multa.setVehiculo(vehiculo);
+        multa.setInfractor(infractorA);
+
+        when(multaRepository.findById(1L)).thenReturn(Optional.of(multa));
+        when(infractorRepository.findById(2L)).thenReturn(Optional.of(infractorB));
+
+        assertThrows(InfractorBloqueadoException.class, () -> multaService.transferirMulta(1L, 2L));
+
+        verify(multaRepository, never()).save(any(Multa.class));
+
     }
 }
